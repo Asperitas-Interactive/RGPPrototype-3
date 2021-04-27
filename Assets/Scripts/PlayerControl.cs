633134aa806d307bf5a8ed3007a41c7f85ceea0f
Pikaboi <braden.cleur@gmail.com>
2021-04-27 13:42:14 +1200

    Revert "uh"
    This reverts commit 234b736564d7537b0f149d874af35f65e376f49e.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -9,12 +10,37 @@ public class PlayerControl : MonoBehaviour
     [SerializeField]
     float speed, gravity;
 
+    /*Scene PredictionScene;
+    PhysicsScene PredictionPhysScene;
+    Scene CurrentScene;
+    PhysicsScene CurrentPhysScene;
+    GameObject clone;
+    public LineRenderer lr;*/
+
     private Vector3 Velocity = new Vector3(0.0f, 0.0f, 0.0f);
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+
+        /*CreateSceneParameters parameters = new CreateSceneParameters(LocalPhysicsMode.Physics3D);
+        PredictionScene = SceneManager.CreateScene("PhysPredict", parameters);
+        PredictionPhysScene = PredictionScene.GetPhysicsScene();
+        CurrentScene = SceneManager.GetActiveScene();
+        CurrentPhysScene = CurrentScene.GetPhysicsScene();
+        Physics.autoSimulation = false;
+
+
+        if (clone == null)
+        {
+            Debug.Log("ah");
+            clone = Instantiate(this.gameObject);
+            SceneManager.MoveGameObjectToScene(clone, PredictionScene);
+        }
+
+        lr = GetComponent<LineRenderer>();
+        lr.positionCount = 100;*/
     }
 
     // Update is called once per frame
@@ -31,4 +57,25 @@ public class PlayerControl : MonoBehaviour
 
         controller.Move(Velocity * Time.deltaTime);
     }
+
+    /*void FixedUpdate()
+    {
+        if (CurrentPhysScene.IsValid())
+        {
+            CurrentPhysScene.Simulate(Time.fixedDeltaTime);
+        }
+
+        Vector3 force = new Vector3(10.0f, 10.0f, 0.0f);
+
+        int Maxiteration = 100;
+
+        clone.transform.position = transform.position;
+        clone.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+
+        for (int i = 0; i < Maxiteration; i++)
+        {
+            PredictionPhysScene.Simulate(Time.fixedDeltaTime);
+            lr.SetPosition(i, clone.transform.position);
+        }
+    }*/
 }
